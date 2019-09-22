d9ad3b87a14514684d72154a8e5bde4038af6c7f
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-22 12:41:53 -0400

    Add project files.

---
+++ triangle assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace triangle_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows :");
+            int i = int.Parse(Console.ReadLine());
+            for (int x = i; x >= 1; x--)
+            {
+                for (int y = i; y > x; y--)
+                {
+                    Console.Write("  ");
+                }
+                for (int z= 1; z < 2 * x; z++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
